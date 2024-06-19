f0ad7889c6d1f3d115263b57d9e3e34ab29d148d
phu <phu.npt01@gmail.com>
2024-06-19 14:49:09 +0700

    update MyUserController and Service

--- src/main/java/com/example/shopping_cart/user/MyUserController.java
+++ src/main/java/com/example/shopping_cart/user/MyUserController.java
@@ -1,12 +1,15 @@
 package com.example.shopping_cart.user;
 
 import lombok.RequiredArgsConstructor;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -16,15 +19,24 @@ public class MyUserController {
 
     private final MyUserService myUserService;
 
+    @NotNull
     @GetMapping("")
     private ResponseEntity<?> findAll() {
-        return myUserService.findAll();
+        List<MyUser> myUsers = myUserService.findAll();
+        if (myUsers.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("No users found");
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(myUsers);
     }
 
     @GetMapping("/search/{user-id}")
     public ResponseEntity<?> findById(
             @PathVariable("user-id")UUID id
     ) {
-        return myUserService.findById(id);
+        MyUser myUser = myUserService.findById(id);
+        if (myUser == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("No user found");
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(myUser);
     }
 }
