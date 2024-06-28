852d1ce7102014d8f8f40181d54cb06cc3a51aa6
phu <phu.npt01@gmail.com>
2024-06-28 11:23:01 +0700

    remove unused

--- src/main/java/com/example/shopping_cart/user/MyUserRequestDTO.java
+++ src/main/java/com/example/shopping_cart/user/MyUserRequestDTO.java
@@ -1,11 +1,6 @@
 package com.example.shopping_cart.user;
 
 import com.example.shopping_cart.address.AddressRequestDTO;
-import com.example.shopping_cart.address.AddressResponseDTO;
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
