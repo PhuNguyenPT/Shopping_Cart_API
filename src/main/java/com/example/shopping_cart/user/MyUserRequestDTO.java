5c57daf872fc456e46134bed3da40fe30d246d87
phu <phu.npt01@gmail.com>
2024-06-28 11:20:22 +0700

    change MyUserRequestDTO

--- src/main/java/com/example/shopping_cart/user/MyUserRequestDTO.java
+++ src/main/java/com/example/shopping_cart/user/MyUserRequestDTO.java
@@ -1,5 +1,7 @@
 package com.example.shopping_cart.user;
 
+import com.example.shopping_cart.address.AddressRequestDTO;
+import com.example.shopping_cart.address.AddressResponseDTO;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
@@ -8,14 +10,17 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.math.BigInteger;
+import java.time.LocalDate;
+
 @Getter
 @Setter
 @Builder
 public class MyUserRequestDTO {
-    @NotNull(message = "Page size must not be null")
-    @Min(value = 1) @Max(value = 20)
-    Integer pageSize;
-    @NotNull(message = "Page index must not be null")
-    @Min(value = 1)
-    Integer pageNumber;
+    private final String firstName;
+    private final String lastName;
+    private final LocalDate dateOfBirth;
+    private final BigInteger phoneNumber;
+    private final String email;
+    private final AddressRequestDTO addressRequestDTO;
 }
